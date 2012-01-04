0c461db424310fe04fc307b156d5d236a180f650
Andrew Platt <andrew.d.platt@gmail.com>
2012-01-04 16:41:55 +0000

    Initial import of NWTC Library. This is version v1.04.00d-bjj
    git-svn-id: https://windsvn2.nrel.gov/NWTC_Library/trunk@1 33a3f72e-afca-4cba-af91-dee07167dc2f

---
+++ source/DoubPrec.f90
@@ -0,0 +1,34 @@
+!=======================================================================
+MODULE Precision
+
+
+   ! This module stores constants to specify the KIND of variables.
+
+   ! NOTE: Use compile option "/real_size:64" (or "/4R8") when using ReKi = 8 with the Visual Fortran compilers.
+
+
+IMPLICIT                           NONE
+
+!INTEGER, PARAMETER              :: B1Ki     =  1                                ! Default kind for one-byte numbers.
+!INTEGER, PARAMETER              :: B2Ki     =  2                                ! Default kind for two-byte numbers.
+!INTEGER, PARAMETER              :: B4Ki     =  4                                ! Default kind for four-byte numbers.
+!INTEGER, PARAMETER              :: B8Ki     =  8                                ! Default kind for eight-byte numbers.
+!INTEGER, PARAMETER              :: DbKi     =  8                                ! Default kind for double-precision, floating-point numbers.
+!INTEGER, PARAMETER              :: QuKi     = 16                                ! Kind for 16-byte, floating-point numbers.
+!INTEGER, PARAMETER              :: ReKi     =  8                                ! Default kind for floating-point numbers.
+!INTEGER, PARAMETER              :: SiKi     =  4                                ! Kind for four-byte, floating-point numbers.
+
+INTEGER, PARAMETER              :: B1Ki     = SELECTED_INT_KIND(  2 )           ! Default kind for one-byte numbers.
+INTEGER, PARAMETER              :: B2Ki     = SELECTED_INT_KIND(  4 )           ! Default kind for two-byte numbers.
+INTEGER, PARAMETER              :: B4Ki     = SELECTED_INT_KIND(  9 )           ! Default kind for four-byte numbers.
+INTEGER, PARAMETER              :: B8Ki     = SELECTED_INT_KIND( 18 )           ! Default kind for eight-byte numbers.
+
+!bjj: for programs like Adams that specify "double" in subroutine arguments, DbKi, should be set to ReKi here....
+!We need something that is 8-byte for both SingPrec.f90 and DoubPrec.f90
+INTEGER, PARAMETER              :: DbKi     = SELECTED_REAL_KIND( 20, 500 )     ! Default kind for double-precision, floating-point numbers.
+INTEGER, PARAMETER              :: QuKi     = SELECTED_REAL_KIND( 20, 500 )     ! Kind for 16-byte, floating-point numbers.
+INTEGER, PARAMETER              :: ReKi     = SELECTED_REAL_KIND( 14, 300 )     ! Default kind for floating-point numbers.
+INTEGER, PARAMETER              :: SiKi     = SELECTED_REAL_KIND(  6,  30 )     ! Kind for four-byte, floating-point numbers.
+
+
+END MODULE Precision
