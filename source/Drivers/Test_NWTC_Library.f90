83d7d60f2e1fd77e03990ee2dbb27c45aa8b86a6
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2013-02-25 19:39:11 +0000

    o  I modified WrOver() in SysGnuWin.f90. It previously produced a run-time error if the string being written was 98 characters or longer.
    git-svn-id: https://windsvn2.nrel.gov/NWTC_Library/trunk@91 33a3f72e-afca-4cba-af91-dee07167dc2f

--- source/Drivers/Test_NWTC_Library.f90
+++ source/Drivers/Test_NWTC_Library.f90
@@ -41,6 +41,18 @@ PROGRAM Test_NWTC_Library
    REAL(ReKi)  :: RealReVar      ! a default real variable (different in SingPrec.f90 or DoubPrec.f90)
    REAL(DbKi)  :: RealDbVar      ! a default double variable (different in SingPrec.f90 or DoubPrec.f90)
     
+   
+   REAL(ReKi), ALLOCATABLE  :: RealReAry(:)
+   
+      ! File name
+   CHARACTER(1024) :: InputFileName = 'TestData/TestLibrary_InputFile.txt'
+   
+   INTEGER(IntKi), ALLOCATABLE  :: TempVec(:,:)
+   INTEGER(IntKi), ALLOCATABLE  :: IC(:)
+   integer,parameter ::    NMX = 9
+   integer :: i, j
+
+   
    !...............................................................................................................................    
    ! Initialize the library
    !...............................................................................................................................    
@@ -57,9 +69,7 @@ PROGRAM Test_NWTC_Library
    CALL WrScr( '  SiKi is '//Num2LStr(SiKi)//'-> It should be  4.' )
    CALL WrScr1('  ReKi is '//Num2LStr(ReKi) )
    CALL WrScr( '  DbKi is '//Num2LStr(DbKi) )
-   
-   
-   
+     
    !...............................................................................................................................    
    ! Test NWTC_Num routines: EqualRealNos 
    !...............................................................................................................................    
@@ -108,6 +118,104 @@ PROGRAM Test_NWTC_Library
       CALL WrScr( ' '//TRIM(Num2LStr(Real4Var))//' is not equal to '//Num2LStr(RealSiVar) )
    END IF  
    
+   
+   !...............................................................................................................................    
+   ! Test Formatted Input File:
+   !...............................................................................................................................    
+   
+   !CALL OpenFInpFile( )
+   
+!INTEGER(IntKi)  :: TempVec(3,2)
+   allocate( TempVec(3,2) )
+   
+   TempVec(:,1) = (/ 1, 2, 3 /)
+   TempVec(:,2) = TempVec(:,1)*6
+   
+   print *, TempVec
+   
+   print *, 'SIZE = ', SIZE(TempVec)
+   
+   print *, PACK(TempVec,.TRUE.)
+   print *, TempVec(1:SIZE(TempVec),1)
+   
+   print *, TempVec(1:6,1)
+   
+   
+   call  WrScr('  This is line 1.'//CHAR(10)//' This is line 2.')
+   
+
+   call  WrScr(NewLine//'  This is line 1.'//NewLine//'   This is line 2.'//NewLine//'  This is line 3.')
+   CALL WrScr(NewLine//NewLine//NewLine//'This is really important'//NewLine//NewLine//&
+        '   This is line 2 a really long line that i need to be more than 98 characters so that it will print on two lines. is this long enough, yet. I''m not sure' )
+   
+   call wrscr('')
+   call wrscr(' one line' )
+   call wrscr('  two lines.')
+   
+   do i=-1,6
+      call wrscr( GetErrStr(i) )
+   end do
+   
+   
+   deallocate(TempVec)
+   
+   
+   print *, 'This is the size of a deallocated array: ', size(TempVec), allocated(TempVec)
+   print *, 'This is the size of a deallocated array: ', size(RealReAry), allocated(RealReAry)
+   
+   RealR8Var = 0.00000001_DbKi
+   print *, MOD( NINT(RealR8Var), 10 ) == 1 
+   RealR8Var = 1.00000001_DbKi  
+   print *, MOD( NINT(RealR8Var), 10 ) == 1 
+   RealR8Var = 10.00000001_DbKi  
+   print *, MOD( NINT(RealR8Var), 10 ) == 1 
+   RealR8Var = 10.00000001_DbKi  + 1.0
+   print *, MOD( NINT(RealR8Var), 10 ) == 1 
+   
+   
+   
+!----------------------------   
+   !allocate( IC( NMX ) )
+   !
+   !IC(1) = 1
+   !DO I = 2,NMX
+   !   IC(I) = IC(1) - I + 1 + NMX
+   !ENDDO
+   !
+   !write( *, '( A, I1, A, '//TRIM(Num2LStr(NMX))//'(I2,1X) )') 'SHIFT ', 0, ': ', IC
+   !
+   !DO j = 1,10
+   !
+   !   IC(1) = IC(1) + 1
+   !   IF ( IC(1) > NMX )  IC(1) = IC(1) - NMX
+   !   DO I = 2,NMX
+   !      IC(I) = IC(1) - I + 1
+   !      IF ( IC(I) <= 0 )  IC(I) = IC(I) + NMX
+   !   ENDDO   
+   !
+   !   write ( *, '( A, I1, A, '//TRIM(Num2LStr(NMX))//'(I2,1X) )') 'SHIFT ', j, ': ', IC
+   !
+   !end do
+   !
+   !print *, ' '
+   !
+   !IC(1) = 1
+   !DO I = 2,NMX
+   !   IC(I) = IC(1) - I + 1 + NMX
+   !ENDDO
+   !
+   !write( *, '( A, I1, A, '//TRIM(Num2LStr(NMX))//'(I2,1X) )') 'SHIFT ', 0, ': ', IC
+   !
+   !DO j = 1,10
+   !
+   !   IC = CSHIFT( IC, -1 )      
+   !   write( *, '( A, I1, A, '//TRIM(Num2LStr(NMX))//'(I2,1X) )') 'SHIFT ', j, ': ', IC
+   !
+   !
+   !end do
+   !
+   !deallocate( IC )
+!----------------------------   
    
    
 END PROGRAM Test_NWTC_Library
