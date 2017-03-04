2025cbb83eb7cdad5412677c6ef75f2221f8b784
Bonnie Jonkman <bonnie.jonkman@envision-energy.com>
2017-03-03 21:05:21 -0700

    fixed optional printing of turbine # in SimStatus

--- source/NWTC_Num.f90
+++ source/NWTC_Num.f90
@@ -5169,7 +5169,7 @@ end function Rad2M180to180Deg
 
       BlankLine = ""
       CALL WrOver( BlankLine )  ! BlankLine contains MaxWrScrLen spaces
-      CALL WrOver ( ' Timestep: '//TRIM( Num2LStr( NINT( ZTime ) ) )//' of '//TRIM( Num2LStr( TMax ) )// &
+      CALL WrOver ( trim(DescStr)//' Timestep: '//TRIM( Num2LStr( NINT( ZTime ) ) )//' of '//TRIM( Num2LStr( TMax ) )// &
                     ' seconds. Estimated final completion at '//ETimeStr//'.'                             )
 
          ! Let's save this time as the previous time for the next call to the routine
